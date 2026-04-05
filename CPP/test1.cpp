8014c74a1b75294a225fd5f754970f7bf9524fd1
John Patrick Cabilan <cabilanjohnpatrick@gmail.com>
2026-04-06 04:59:41 +0800

    added great function in cpp

--- CPP/test1.cpp
+++ CPP/test1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void greet() {
+    cout << "Keep going, you're improving!" << endl;
+}
+
 int main() {
     cout << "Hello from C++!" << endl;
     cout << "Practicing Git with C++" << endl;
+    greet();
     return 0;
 }
